7785b14e683f368f50e3e11b54409b4c1587fcbc
Alberto Alvarez Garcia <aag_uk@yahoo.com>
2020-08-29 03:29:51 -0400

    local socket queue

---
+++ src/cft_fw/cft_queues/cft_local_socket.c
@@ -0,0 +1,97 @@
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#include "cft_local_socket.h"
+#include "cft_queues.h"
+
+bool cft_local_socket_server_init(cft_local_socket_server_t* server_socket, const char *name)
+{
+    memset(server_socket, 0, sizeof(*server_socket));
+    strcpy(server_socket->name_, "/tmp/");
+    strcat(server_socket->name_, name);
+    unlink(server_socket->name_);
+
+    struct sockaddr_un address;
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, server_socket->name_);
+
+    if((server_socket->descriptor_ = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+        return false;
+    }
+    // Bind the socket to the address.
+    if ((bind(server_socket->descriptor_, (struct sockaddr *)&address, sizeof(address))) != 0) {
+        return false;
+    }
+
+    if ((listen(server_socket->descriptor_, 100)) != 0) {
+        return false;
+    }
+
+    struct timeval tv;
+    tv.tv_sec = 2;
+    tv.tv_usec = 0;
+    setsockopt(server_socket->descriptor_, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv);
+    // Accept incoming connections.
+    if((server_socket->connection_ = accept(server_socket->descriptor_, (struct sockaddr*)NULL, NULL)) < 0) {
+        return false;
+    }
+
+    return true;
+
+}
+
+bool cft_local_socket_client_init(cft_local_socket_client_t* client_socket, const char *name)
+{
+    memset(client_socket, 0, sizeof(*client_socket));
+
+    struct sockaddr_un address;
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, "/tmp/");
+    strcat(address.sun_path, name);
+
+    if((client_socket->descriptor_ = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
+        return false;
+    }
+
+    while(connect(client_socket->descriptor_ , (struct sockaddr *)&address, sizeof(address)) != 0) {
+        sleep(1);
+    }
+
+    return true;
+}
+
+void cft_local_socket_server_fini(cft_local_socket_server_t* server_socket)
+{
+    close(server_socket->connection_);
+    close(server_socket->descriptor_);
+    unlink(server_socket->name_);
+}
+
+void cft_local_socket_client_fini(cft_local_socket_client_t* client_socket)
+{
+    shutdown(client_socket->descriptor_, SHUT_RDWR);
+    close(client_socket->descriptor_);
+}
+
+int cft_local_socket_server_read(cft_local_socket_server_t* socket, void* buffer, size_t size)
+{
+    return read(socket->connection_, buffer, size);
+}
+
+int cft_local_socket_server_write(cft_local_socket_server_t* socket, void* buffer, size_t size)
+{
+    return write(socket->connection_, buffer, size);
+}
+
+int cft_local_socket_client_read(cft_local_socket_client_t* socket, void* buffer, size_t size)
+{
+    return read(socket->descriptor_, buffer, size);
+}
+
+int cft_local_socket_client_write(cft_local_socket_client_t* socket, void* buffer, size_t size)
+{
+    return write(socket->descriptor_, buffer, size);
+}
+
---
+++ src/cft_fw/cft_queues/cft_local_socket.h
@@ -0,0 +1,33 @@
+#ifndef CFT_FW_CFT_QUEUES_CFT_LOCAL_SOCKET_H_
+#define CFT_FW_CFT_QUEUES_CFT_LOCAL_SOCKET_H_
+
+#include "common_c.h"
+#include "cft_queues.h"
+
+typedef int cft_local_socket_descriptor_t;
+
+struct cft_local_socket_client_s {
+    cft_local_socket_descriptor_t descriptor_;
+};
+
+struct cft_local_socket_server_s {
+    char name_[QUEUE_NAME_MAX_SIZE];
+    cft_local_socket_descriptor_t descriptor_;
+    int connection_;
+};
+
+
+typedef struct cft_local_socket_client_s cft_local_socket_client_t;
+typedef struct cft_local_socket_server_s cft_local_socket_server_t;
+
+bool cft_local_socket_server_init(cft_local_socket_server_t* socket, const char *name);
+void cft_local_socket_server_fini(cft_local_socket_server_t* socket);
+int cft_local_socket_server_read(cft_local_socket_server_t* socket, void* buffer, size_t size);
+int cft_local_socket_server_write(cft_local_socket_server_t* socket, void* buffer, size_t size);
+
+bool cft_local_socket_client_init(cft_local_socket_client_t* socket, const char *name);
+void cft_local_socket_client_fini(cft_local_socket_client_t* socket);
+int cft_local_socket_client_read(cft_local_socket_client_t* socket, void* buffer, size_t size);
+int cft_local_socket_client_write(cft_local_socket_client_t* socket, void* buffer, size_t size);
+
+#endif /* CFT_FW_CFT_QUEUES_CFT_LOCAL_SOCKET_H_ */
